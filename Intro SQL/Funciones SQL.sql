b25d7eddba08be3d734685329827aea2fff540cc
jvelosa <jrvelosa@misena.edu.co>
2019-07-14 21:50:44 -0500

    Mi Primer Paquete
    PLSQL

---
+++ Intro SQL/Funciones SQL.sql
@@ -0,0 +1,22 @@
+/********************* SELECT************************/
+
+SELECT * FROM HR.JOBS;
+
+SELECT * FROM HR.REGIONS
+
+/********************* TABLA DUAL************************/
+
+SELECT * FROM DUAL;
+
+SELECT (5*50) AS "MULTI DUAL" FROM DUAL;
+
+SELECT 'Hola Rodrigo' as saludo from dual;
+
+select sysdate from dual;
+
+select user from dual;
+
+/********************* FUNCION CONCAT************************/
+
+
+
